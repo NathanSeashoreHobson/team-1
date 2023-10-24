c81a5c411d9fbc5c5130e3e156ffa724019bedb1
Nathan Seashore Hobson <46633219+NathanSeashoreHobson@users.noreply.github.com>
2023-10-24 10:37:00 -0400

    Comment sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -2,16 +2,20 @@
 
 int main()
 {
+    // Set initial Fibonacci Numbers
     int a = 0, b = 1;
     int sum_even = 0;
+    // Sum even Fibonacci Numbers
     while (b < 4000000) {
         if (b % 2 == 0) {
             sum_even += b;
         }
+        // Update Fibonacci Numbers
         int temp = b;
         b = a + b;
         a = temp;
     }
+    // Print out sum
     std::cout << sum_even << std::endl;
 
     return 0;
